804379797b838d782aa1e594c411a4651c3e1580
Jeremy Woertink <jeremywoertink@gmail.com>
2012-02-09 19:29:21 -0800

    removed array class in favor of built in shuffle. fixed delete name bugs

--- rubybooty.rb
+++ rubybooty.rb
@@ -1,14 +1,9 @@
 module Rubybooty
-  class Array
-    def shuffle!
-      size.downto(1) { |n| push delete_at(rand(n)) }
-      self
-    end
-  end
   
   def self.raffle_sort(entrants)
     entries = []
     entrants.each { |k,v| v.times { entries << k }}
-    winner = entries.shuffle!.first
+    rand(100).times { entries.shuffle! }
+    winner = entries.first
   end
 end
--- run.rb
+++ run.rb
@@ -7,8 +7,13 @@ require_relative 'rubybooty'
 include Rubybooty
 Shoes.app do
   @entrants = {}
+  @checks = {}
+  @names_list = []
+  @para_list = []
+  i = 0
+  
   banner "RubyBooty"
-  stack width: "70%" do
+  stack width: "60%" do
     flow do
       para "Entrants name: "
       @name = edit_line
@@ -17,10 +22,28 @@ Shoes.app do
       para "Number of entries: "
       @entries = edit_line(width: 50)
     end
+    
     button "Add" do
       @entrants[@name.text] = @entries.text.to_i
       @name_list.append do
-        para "#{@name.text} is in for $#{@entrants[@name.text]}"
+        @para_list.push << flow do
+          para "#{@name.text} is in for $#{@entrants[@name.text]}"
+          inscription "Delete?"
+          @check = check
+        end
+        @names_list << @name.text
+        @check.click do |c|
+          if c.checked? and confirm('Are you sure?')
+            val = @checks.values_at(c).first
+            @entrants.delete_if { |k,v| k.eql?(@names_list[val]) }
+            @para_list[val].remove
+            @sum = @entrants.values.inject(0, &:+)
+            @total.text = "Total: $#{@sum}"
+          end
+          c.checked = false
+        end
+        @checks[@check] = i
+        i += 1
       end
       @sum = @entrants.values.inject(0, &:+)
       @total.text = "Total: $#{@sum}"
@@ -28,9 +51,10 @@ Shoes.app do
       @entries.text = ""
     end
   end
-  stack width: "30%" do
+  stack width: "40%" do
     stack do
       background white
+      border black, strokewidth: 1
       @name_list = stack
     end
     stack do
