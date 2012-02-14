a4c89c6cfa00b804635993a5761c1d0364f8504d
Jeremy Woertink <jeremywoertink@gmail.com>
2012-02-14 13:13:53 -0800

    added some styles to the interface

--- run.rb
+++ run.rb
@@ -7,8 +7,10 @@ Shoes.app(title: 'LVRUG Rubybooty', width: 760) do
   @names_list = []
   @para_list = []
   i = 0
-  
-  banner "RubyBooty"
+  flow width: "100%", margin: ["0px", "0px", "0px", "10%"] do
+    background '#8D0000'..'#000000', curve: 12
+    banner "RubyBooty", align: "center", stroke: white
+  end
   stack width: "60%" do
     flow do
       para "Entrants name: "
@@ -62,12 +64,27 @@ Shoes.app(title: 'LVRUG Rubybooty', width: 760) do
       winner = Rubybooty.raffle_sort(@entrants)
       if winner
         @results.append do
-          title "#{winner} is the winner of $#{(@sum / 2.0).ceil}! AARRR!"
+          @title = para
+          @title.style(font: "Arial 40px", stroke: red)
+          @title.text = "and the winner is"
+          @anim = animate(30) do |a|
+            @title.text += "."
+            if a >= 40
+              @title.text = "#{winner} for $#{(@sum / 2.0).ceil}! AARRR!"
+              @anim.stop
+            end
+          end
         end
       else
         alert "No winner. :("
       end
     end
     @results = stack
+  end
+  flow width: "100%", margin: ["0px", "10%", "0px", "0px"] do
+    background '#CACACA'
+    stack top: 0, left: ((self.width - 120) / 2) do
+      image File.join(Dir.pwd, 'assets', 'lvrug.png'), width: 120, height: 63, click: "http://www.meetup.com/las-vegas-ruby-on-rails/"
+    end
   end
 end
